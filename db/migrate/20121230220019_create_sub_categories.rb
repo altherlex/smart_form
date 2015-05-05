972ec196c8ae1026afa74f8f03b7cd5f273b9e9c
Altherlex <para.alves@gmail.com>
2015-05-05 13:39:39 -0300

    for heroku

--- db/migrate/20121230220019_create_sub_categories.rb
+++ db/migrate/20121230220019_create_sub_categories.rb
@@ -2,7 +2,6 @@ class CreateSubCategories < ActiveRecord::Migration
   def change
     create_table :sub_categories do |t|
       t.references :category
-      t.text :title
       t.text :name
       t.text :slug, :unique => true, :null => false
       t.text :fields
